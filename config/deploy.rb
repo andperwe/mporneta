84a79b412bfd7e3ec6cef12dd271ba594537813d
andperwe <andrzej.perwejnis@gmail.com>
2017-01-04 20:41:41 +0100

    mwlubawa

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :passenger_restart_with_touch, true
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/agent_user/mporneta'
+# zmienić mporneta
+set :deploy_to, '/home/agent_user/mwlubawa'
 
 # Default value for :scm is :git
 # set :scm, :git
